2301f337be120fcf2a05953e7b07f34ab7574e8c
Harri Lainio <harri.lainio@iki.fi>
2022-01-22 18:40:37 +0200

    updating try packages documentation

--- try/try.go
+++ try/try.go
@@ -1,6 +1,21 @@
+// Package try is a new sub package for `try to` functions which replace all of
+// the error checking. The error handlers still stay in the main package err2.
+// For more information see FileCopy example in err2:
+//  ...
+//  r := try.To1(os.Open(src))
+//  defer r.Close()
+//
+//  w := try.To1(os.Create(dst))
+//  defer err2.Handle(&err, func() {
+//  	os.Remove(dst)
+//  })
+//  defer w.Close()
+//  try.To1(io.Copy(w, r))
+//  return nil
+//  ...
 package try
 
-// To is a helper method to call functions which returns (error)
+// To is a helper function to call functions which returns (error)
 // and check the error value. If error occurs it panics the error where err2
 // handlers can catch it if needed.
 func To(err error) {
@@ -9,7 +24,7 @@ func To(err error) {
 	}
 }
 
-// To1 is a helper method to call functions which returns (any, error)
+// To1 is a helper function to call functions which returns (any, error)
 // and check the error value. If error occurs it panics the error where err2
 // handlers can catch it if needed.
 func To1[T any](v T, err error) T {
@@ -17,7 +32,7 @@ func To1[T any](v T, err error) T {
 	return v
 }
 
-// To2 is a helper method to call functions which returns (any, any, error)
+// To2 is a helper function to call functions which returns (any, any, error)
 // and check the error value. If error occurs it panics the error where err2
 // handlers can catch it if needed.
 func To2[T, U any](v1 T, v2 U, err error) (T, U) {
@@ -25,7 +40,7 @@ func To2[T, U any](v1 T, v2 U, err error) (T, U) {
 	return v1, v2
 }
 
-// To3 is a helper method to call functions which returns (any, any, any, error)
+// To3 is a helper function to call functions which returns (any, any, any, error)
 // and check the error value. If error occurs it panics the error where err2
 // handlers can catch it if needed.
 func To3[T, U, V any](v1 T, v2 U, v3 V, err error) (T, U, V) {
