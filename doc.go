21d1139a1593ad72f1af19eb30bb18a5f3b1b3d0
Harri Lainio <harri.lainio@iki.fi>
2022-04-10 18:08:24 +0300

    err2 doc has fully commented CopyFile example

--- doc.go
+++ doc.go
@@ -14,27 +14,39 @@ The err2 package drives programmers to focus more on error handling rather than
 checking errors. We think that checks should be so easy that we never forget
 them. The CopyFile example shows how it works:
 
+ // CopyFile copies source file to the given destination. If any error occurs it
+ // returns error value describing the reason.
  func CopyFile(src, dst string) (err error) {
+      // Add first error handler just to annotate the error properly.
       defer err2.Returnf(&err, "copy %s %s", src, dst)
 
-      // These try package helpers are as fast as Check() calls which is as
-      // fast as `if err != nil {}`
-
+      // Try to open the file. If error occurs now, err will be annotated and
+      // returned properly thanks to above err2.Returnf.
       r := try.To1(os.Open(src))
       defer r.Close()
 
+      // Try to create a file. If error occurs now, err will be annotated and
+      // returned properly.
       w := try.To1(os.Create(dst))
+      // Add error handler to clean up the destination file. Place it here that
+      // the next deferred close is called before our Remove call.
       defer err2.Handle(&err, func() {
       	os.Remove(dst)
       })
       defer w.Close()
+
+      // Try to copy the file. If error occurs now, all previous error handlers
+      // will be called in the reversed order. And final return error is
+      // properly annotated in all the cases.
       try.To1(io.Copy(w, r))
+	 
+      // All OK, just return nil.
       return nil
  }
 
 Error checks
 
-The `try` package provides convenient helpers to check the errors. For example,
+The try package provides convenient helpers to check the errors. For example,
 instead of
 
  b, err := ioutil.ReadAll(r)
