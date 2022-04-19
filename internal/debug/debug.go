4f0a37774d562c53345dc73028f71182ae1a0994
Harri Lainio <harri.lainio@iki.fi>
2022-04-19 17:29:58 +0300

    new stack printing func for specific writer

--- internal/debug/debug.go
+++ internal/debug/debug.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"runtime/debug"
 )
@@ -11,12 +12,19 @@ import (
 // PrintStack prints to standard error the stack trace returned by runtime.Stack
 // by starting from stackLevel.
 func PrintStack(stackLevel int) {
+	FprintStack(os.Stderr, stackLevel)
+}
+
+// FprintStack prints to standard error the stack trace returned by runtime.Stack
+// by starting from stackLevel.
+func FprintStack(w io.Writer, stackLevel int) {
 	stackBuf := bytes.NewBuffer(debug.Stack())
 	scanner := bufio.NewScanner(stackBuf)
 
-	for i := 0; scanner.Scan(); i++ {
-		if i/2 > stackLevel {
-			fmt.Fprintln(os.Stderr, scanner.Text())
+	// there is a caption line first, that's why we start from -1
+	for i := -1; scanner.Scan(); i++ {
+		if i == -1 || i/2 >= stackLevel {
+			fmt.Fprintln(w, scanner.Text())
 		}
 	}
 }
