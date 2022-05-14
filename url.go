d9929d785a90e78b93acc20cf416a6fcf92a0db6
Harri Lainio <harri.lainio@iki.fi>
2022-05-14 19:27:08 +0300

    added back to system as deprecated

---
+++ url.go
@@ -0,0 +1,17 @@
+package err2
+
+import "net/url"
+
+type _URL struct{}
+
+// URL is a helper variable to generated
+// 'type wrappers' to make Try function as fast as Check.
+// Note! Deprecated, use try package.
+var URL _URL
+
+// Try is a helper method to call func() (*url.URL, error) functions
+// with it and be as fast as Check(err).
+func (o _URL) Try(v *url.URL, err error) *url.URL {
+	Check(err)
+	return v
+}
