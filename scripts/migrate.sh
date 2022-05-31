14b017eeeda929ad7486f3a2cf6aa1f9df78a83d
Harri Lainio <harri.lainio@iki.fi>
2022-05-31 15:57:52 +0300

    migration script going forward

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -3,34 +3,38 @@
 location=$(dirname "$BASH_SOURCE")
 echo $location
 
-# # Replace FilterTry with our new version
-# "$location"/replace.sh 'err2\..*FilterTry\(' 'try.Is('
-# 
-# # Replace IsEOF with our new version
-# "$location"/replace.sh 'err2\.TryEOF\(' 'try.IsEOF('
-# 
-# # replace StrStr as it own because it returns two values
-# "$location"/replace.sh 'err2\.StrStr\.Try\(' 'try.To2('
-# 
-# # change all the rest type variable usages.
-# # todo: if you have your own of them like e2.XxxxType.Try use this as guide
-# "$location"/replace.sh 'err2\..*Try\(' 'try.To1('
-# 
-# # add try import
-# "$location"/replace.sh '\"github.com\/lainio\/err2\"' '\"github.com\/lainio\/err2\"\n\t\"github.com\/lainio\/err2\/try\"\n' 
-# 
-# "$location"/replace.sh '(err2.Check\()(.*)(\))' 'try.To(\2\3'
-# 
+# Replace FilterTry with our new version
+"$location"/replace.sh 'err2\..*FilterTry\(' 'try.Is('
+
+# Use IsEOF instead of TryEOF
+"$location"/replace.sh 'err2\.TryEOF\(' 'try.IsEOF('
+
+# replace StrStr as it own because it returns two values
+"$location"/replace.sh 'err2\.StrStr\.Try\(' 'try.To2('
+
+# change all the rest type variable usages.
+# todo: if you have your own of them like e2.XxxxType.Try use this as guide
+"$location"/replace.sh 'err2\..*Try\(' 'try.To1('
+
+"$location"/replace.sh '(err2.Check\()(.*)(\))' 'try.To(\2\3'
+
+# add try import
+#"$location"/replace.sh '(try\.To|try\.Is)' '\"github.com\/lainio\/err2\"' '\"github.com\/lainio\/err2\"\n\t\"github.com\/lainio\/err2\/try\"' 
+# ============
+# == ff here =
+# ============
+
 # 5 ok:
 #use_perl=perl "$location"/replace.sh '(, \w*)(, err)( :?= )([\w\(\)\[\],\. ]*)(\n)(\s*try.To\(err\))' '\1\3try.To2(\4)'
 #"$location"/replace-perl.sh '(, \w*)(, err)( :?= )([\w\(\)\[\],\. "]*)(\n)(\s*try.To\(err\))' '\1\3try.To2(\4)'
-# NEW, latest version
-"$location"/replace-perl.sh '(, \w*)(, err)( :?= )([\w\s\.,:;%&=\-\(\)\{\}\[\]\$\^\?\\\|\+\"\*]*)(\n)(\s*try\.To\(err\))' '\1\3try.To2(\4)'
+# NEW, latest version ====== this the SECOND
+#"$location"/replace-perl.sh '(, \w*)(, err)( :?= )([\w\s\.,:;%&=\-\(\)\{\}\[\]\$\^\?\\\|\+\"\*]*)(\n)(\s*try\.To\(err\))' '\1\3try.To2(\4)'
 
 # '(, \w*)(, err)( :?= )([\w\s\.,:;%&\-\(\){}\[\]\$\^\?\\\|\+\"\*]*)(\s*try\.To\(err\))'
 # 6 ok:
 #"$location"/replace-perl.sh '(, err)( :?= )([\w\s\.,"\(\)\{\}\[\]\*]*)(\n)(\s*try.To\(err\))' '\2try.To1(\3)'
 #"$location"/replace-perl.sh '(, err)( :?= )([\w\s\.,:;%&-\*\(\)\{\}\[\]\$\^\?\\\|\+\"]*)(\n)(\s*try.To\(err\))' '\2try.To1(\3)'
+"$location"/replace-perl.sh '(, err)( :?= )([\w\s\.,:;%&=\-\(\)\{\}\[\]\$\^\?\\\|\+\"\*]*)(\n)(\s*try\.To\(err\))' '\2try.To1(\3)'
 
 # err :?=[\w\s\.,\(\)\{\}\[\]\*]*try\.To
 # cleanup and add needed imports
