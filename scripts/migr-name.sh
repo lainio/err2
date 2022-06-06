cc9dd3f96cb02e4766faa2242070be7412a27df3
Harri Lainio <harri.lainio@iki.fi>
2022-06-06 14:22:30 +0300

    migr-name not uses no_build_check anymore

--- scripts/migr-name.sh
+++ scripts/migr-name.sh
@@ -5,7 +5,6 @@ location=$(dirname "$BASH_SOURCE")
 
 start_branch=$(git rev-parse --abbrev-ref HEAD)
 migration_branch=${migration_branch:-"err2-update"}
-no_build_check=${no_build_check:-"1"}
 use_current_branch=${use_current_branch:-"1"}
 
 echo "location: $location"
