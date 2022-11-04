6322dc5227da4dec44dfdec6be76a46b0b9aef14
Harri Lainio <harri.lainio@iki.fi>
2022-11-04 13:53:28 +0200

    most of the handler.Process tests are ready

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -3,6 +3,7 @@ package handler_test
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/lainio/err2/internal/handler"
@@ -22,86 +23,127 @@ func TestProcess(t *testing.T) {
 		name string
 		args args
 	}{
-		{"all nil", args{Info: handler.Info{
-			Any:          nil,
-			Err:          &myErrNil,
-			NilHandler:   nilHandler,
-			ErrorHandler: errorHandler,
-			PanicHandler: panicHandler,
-		},
-			panicCalled: false,
-			errorCalled: false,
-			nilCalled:   false,
-			errStr:      "error",
-		}},
-		{"error in panic", args{Info: handler.Info{
-			Any:          errors.New("error"),
-			Err:          &myErrNil,
-			NilHandler:   nilHandler, // can be here?
-			ErrorHandler: errorHandler,
-			PanicHandler: panicHandler,
-		},
-			panicCalled: false,
-			errorCalled: true,
-			nilCalled:   false,
-			errStr:      "error",
-		}},
-		{"error in panic and default format print", args{Info: handler.Info{
-			Any:        errors.New("error"),
-			Err:        &myErrNil,
-			Format:     "format %v",
-			Args:       []any{"test"},
-			NilHandler: nilHandler, // can be here?
-			//ErrorHandler: errorHandler,
-			PanicHandler: panicHandler,
-		},
-			panicCalled: false,
-			//errorCalled: true,
-			nilCalled: false,
-			errStr:    "error",
-		}},
-		{"error in error", args{Info: handler.Info{
-			Any: nil,
-			Err: &myErrVal,
-			//NilHandler:   nilHandler, // cannot be here
-			ErrorHandler: errorHandler,
-			PanicHandler: panicHandler,
-		},
-			panicCalled: false,
-			errorCalled: true,
-			nilCalled:   false,
-			errStr:      "error",
-		}},
+		{"all nil and our handlers",
+			args{Info: handler.Info{
+				Any:          nil,
+				Err:          &nilError,
+				NilHandler:   nilHandler,
+				ErrorHandler: errorHandler,
+				PanicHandler: panicHandler,
+			},
+				errStr: "error",
+			}},
+		{"error is transported in panic",
+			args{Info: handler.Info{
+				Any:          errors.New("error"),
+				Err:          &nilError,
+				ErrorHandler: errorHandler,
+				PanicHandler: panicHandler,
+			},
+				panicCalled: false,
+				errorCalled: true,
+				errStr:      "error",
+			}},
+		{"runtime.Error is transported in panic",
+			args{Info: handler.Info{
+				Any:          myErrRT,
+				Err:          &nilError,
+				NilHandler:   nilHandler,
+				ErrorHandler: errorHandler,
+				PanicHandler: panicHandler,
+			},
+				panicCalled: true,
+				errStr:      "error",
+			}},
+		{"panic is transported in panic",
+			args{Info: handler.Info{
+				Any:          "panic",
+				Err:          &nilError,
+				NilHandler:   nilHandler,
+				ErrorHandler: errorHandler,
+				PanicHandler: panicHandler,
+			},
+				panicCalled: true,
+				errStr:      "error",
+			}},
+		{"error in panic and default format print",
+			args{Info: handler.Info{
+				Any:          errors.New("error"),
+				Err:          &nilError,
+				Format:       "format %v",
+				Args:         []any{"test"},
+				PanicHandler: panicHandler,
+			},
+				panicCalled: false,
+				errStr:      "error",
+			}},
+		{"error transported in panic and our OWN handler",
+			args{Info: handler.Info{
+				Any:          errors.New("error"),
+				Err:          &nilError,
+				Format:       "format %v",
+				Args:         []any{"test"},
+				ErrorHandler: errorHandlerForAnnotate,
+				PanicHandler: panicHandler,
+			},
+				panicCalled: false,
+				errorCalled: true,
+				errStr:      "annotate: error",
+			}},
+		{"error is transported in error val",
+			args{Info: handler.Info{
+				Any:          nil,
+				Err:          &myErrVal,
+				ErrorHandler: errorHandler,
+				PanicHandler: panicHandler,
+			},
+				panicCalled: false,
+				errorCalled: true,
+				errStr:      "error",
+			}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			handler.Process(tt.args.Info)
-			helper.Require(t, tt.args.panicCalled == panicHandlerCalled, "panicHandler")
-			helper.Requiref(t, tt.args.errorCalled == errorHandlerCalled, "errHandled = %v", errorHandlerCalled)
-			helper.Require(t, tt.args.nilCalled == nilHandlerCalled, "nilHandler")
+			handler.Process(&tt.args.Info)
 
-			helper.Requiref(t, tt.args.errStr == myErrVal.Error(), "got: %v", myErrVal.Error())
-			resetCalleds()
+			helper.Requiref(t, tt.args.panicCalled == panicHandlerCalled, "panicHandler: got = %v, want = %v", tt.args.panicCalled, panicHandlerCalled)
+			helper.Requiref(t, tt.args.errorCalled == errorHandlerCalled, "errorHandler: got = %v, want = %v", tt.args.errorCalled, errorHandlerCalled)
+			helper.Requiref(t, tt.args.nilCalled == nilHandlerCalled, "nilHandler: got = %v, want = %v", tt.args.nilCalled, nilHandlerCalled)
+
+			helper.Requiref(t, tt.args.errStr == myErrVal.Error(),
+				"got: %v, want: %v", myErrVal.Error(), tt.args.errStr)
+
+			resetCalled()
 		})
 	}
 }
 
+type myRuntimeErr struct{}
+
+func (rte myRuntimeErr) RuntimeError() {}
+func (rte myRuntimeErr) Error() string { return "runtime error" }
+
 var (
 	errVal = errors.New("error")
+	errRT  = new(myRuntimeErr)
 )
 
 var (
-	myErrNil error = nil
-	myErrVal       = errVal
+	// Important 'cause our errors are ptrs to error interface
+	nilError error = nil
+
+	myErrVal = errVal
+	myErrRT  = errRT
 
 	panicHandlerCalled = false
 	errorHandlerCalled = false
 	nilHandlerCalled   = false
 )
 
-func resetCalleds() {
-	myErrNil = nil
+func resetCalled() {
+	nilError = nil
 	myErrVal = errVal
+	myErrRT = errRT
 	panicHandlerCalled = false
 	errorHandlerCalled = false
 	nilHandlerCalled = false
@@ -109,6 +151,11 @@ func resetCalleds() {
 
 func panicHandler(_ any) {
 	panicHandlerCalled = true
+}
+
+func errorHandlerForAnnotate(err error) {
+	errorHandlerCalled = true
+	myErrVal = fmt.Errorf("annotate: %v", err)
 }
 
 func errorHandler(_ error) {
