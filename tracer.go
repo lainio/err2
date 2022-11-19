4b69bf56445b484fbb3f5a3a6d4490838f089644
Harri Lainio <harri.lainio@iki.fi>
2022-11-19 13:20:10 +0200

    documentation where error stack printing is done

--- tracer.go
+++ tracer.go
@@ -20,20 +20,28 @@ func PanicTracer() io.Writer {
 
 // SetErrorTracer sets a io.Writer for automatic error stack tracing. Note
 // that runtime.Error types which are transported by panics are controlled by
-// this.
+// this. Note also that the current function is capable to print error stack
+// trace when the function has at least one deferred error handler, e.g:
+//
+//	func CopyFile(src, dst string) (err error) {
+//	     defer err2.Handle(&err) // <- error trace print decision is done here
 func SetErrorTracer(w io.Writer) {
 	tracer.Error.SetTracer(w)
 }
 
 // SetPanicTracer sets a io.Writer for automatic panic stack tracing. Note
 // that runtime.Error types which are transported by panics are controlled by
-// this.
+// this. Note also that the current function is capable to print panic stack
+// trace when the function has at least one deferred error handler, e.g:
+//
+//	func CopyFile(src, dst string) (err error) {
+//	     defer err2.Handle(&err) // <- error trace print decision is done here
 func SetPanicTracer(w io.Writer) {
 	tracer.Panic.SetTracer(w)
 }
 
 // SetTracers a convenient helper to set a io.Writer for error and panic stack
-// tracing.
+// tracing. More information see SetErrorTracer and SetPanicTracer functions.
 func SetTracers(w io.Writer) {
 	tracer.Error.SetTracer(w)
 	tracer.Panic.SetTracer(w)
