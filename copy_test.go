910d7cabdb592d1d617c28f1559488cf12a882bf
Harri Lainio <harri.lainio@iki.fi>
2023-02-19 20:34:26 +0200

    linter for new bench

--- copy_test.go
+++ copy_test.go
@@ -17,7 +17,6 @@ func Benchmark_CopyBufferStd(b *testing.B) {
 	helper.Require(b, all != nil)
 
 	buf := make([]byte, 4)
-	
 	dst := bufio.NewWriter(bytes.NewBuffer(make([]byte, 0, len(all))))
 	src := bytes.NewReader(all)
 	for n := 0; n < b.N; n++ {
@@ -31,12 +30,11 @@ func Benchmark_CopyBufferOur(b *testing.B) {
 	helper.Require(b, all != nil)
 
 	tmp := make([]byte, 4)
-	
 	dst := bufio.NewWriter(bytes.NewBuffer(make([]byte, 0, len(all))))
 	src := bytes.NewReader(all)
 	for n := 0; n < b.N; n++ {
 		for eof, n := try.IsEOF1(src.Read(tmp)); !eof; eof, n = try.IsEOF1(src.Read(tmp)) {
-			dst.Write(tmp[:n])
+			_, _ = dst.Write(tmp[:n])
 		}
 	}
 }
