e145f070d9ade6c6c647bba8a320035edfe0c4fd
Harri Lainio <harri.lainio@iki.fi>
2023-05-14 21:48:46 +0300

    slice reverse added to x pkg

--- internal/x/x.go
+++ internal/x/x.go
@@ -9,6 +9,16 @@ func Whom[T any](b bool, yes, no T) T {
 	return no
 }
 
+func SReverse[T any](in []T) (out []T)  {
+	length := len(in)
+	out = make([]T, length)
+	max := length - 1
+	for i, v := range in {
+		out[max-i] = v
+	}
+	return out
+}
+
 // GetAndSet gets value to the *ptr and returns the old one, after setting new.
 func GetAndSet[T any](ptr *T, new T) (old T) {
 	old = *ptr
