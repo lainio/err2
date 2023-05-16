167e10469e19197e90831dfb1bb6680d104701ea
Harri Lainio <harri.lainio@iki.fi>
2023-05-16 15:10:26 +0300

    new Reverse functions for slide

--- internal/x/x.go
+++ internal/x/x.go
@@ -10,41 +10,21 @@ func Whom[T any](b bool, yes, no T) T {
 }
 
 // SReverse reverse slice order. Doesn't clone slice. This is Fastest of these.
-func SSReverse[S ~[]E, E any](s S) S {
-	for i := len(s)/2 - 1; i >= 0; i-- {
-		opp := len(s) - 1 - i
+func SReverse[S ~[]E, E any](s S) S {
+	length := len(s)
+	for i := length/2 - 1; i >= 0; i-- {
+		opp := length - 1 - i
 		s[i], s[opp] = s[opp], s[i]
 	}
 	return s
 }
 
-// SReverse reverse slice order. Doesn't clone slice. This is Fastest of these.
-func SReverse[S ~[]E, E any](s S) S {
+// SSReverse reverse slice order. Doesn't clone slice. This is Fastest of these.
+func SSReverse[S ~[]E, E any](s S) S {
 	for begin, end := 0, len(s)-1; begin < end; begin, end = begin+1, end-1 {
 		s[begin], s[end] = s[end], s[begin]
 	}
 	return s
-}
-
-func SReverseClone[T ~[]U, U any](in T) (out T) {
-	out = make(T, len(in))
-	//copy(out, in)
-	//SReverse(out)
-	for begin, end := 0, len(in)-1; begin < end; begin, end = begin+1, end-1 {
-		out[begin] = out[end]
-	}
-
-	return out
-}
-
-func OSReverseClone[T ~[]U, U any](in T) (out T) {
-	length := len(in)
-	out = make(T, length)
-	max := length - 1
-	for i, v := range in {
-		out[max-i] = v
-	}
-	return out
 }
 
 // GetAndSet gets value to the *ptr and returns the old one, after setting new.
