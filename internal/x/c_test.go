d267760ae440a03d3f666ca0d56702f004c16235
Harri Lainio <harri.lainio@iki.fi>
2023-05-16 15:18:49 +0300

    tests and bench for x pkg

--- internal/x/c_test.go
+++ internal/x/c_test.go
@@ -1,31 +1,33 @@
 package x
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/lainio/err2/internal/test"
+)
 
 var (
-	lengths = []int{2, 16, 128, 1024, 8192, 65536, 524288, 4194304, 16777216, 134217728}
+	original       = []int{2, 16, 128, 1024, 8192, 65536, 524288, 4194304, 16777216, 134217728}
+	lengths        = []int{2, 16, 128, 1024, 8192, 65536, 524288, 4194304, 16777216, 134217728}
+	reverseLengths = []int{134217728, 16777216, 4194304, 524288, 65536, 8192, 1024, 128, 16, 2}
 )
+
+func TestSReverse(t *testing.T) {
+	SReverse(lengths)
+	test.Require(t, reflect.DeepEqual(lengths, reverseLengths))
+	SReverse(lengths) // it's reverse now turn it to original
+	test.Require(t, reflect.DeepEqual(lengths, original))
+}
 
 func BenchmarkSSReverse(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		SReverse(lengths)
+		SSReverse(lengths)
 	}
 }
 
 func BenchmarkSReverse(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		SReverse(lengths)
-	}
-}
-
-func BenchmarkSReverseClone(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_ = SReverseClone(lengths)
-	}
-}
-
-func BenchmarkXSReverseClone(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_ = SReverseClone(lengths)
 	}
 }
