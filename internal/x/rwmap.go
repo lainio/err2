3d418c9dbd3938dd065b3dc3048addc9230219de
Harri Lainio <harri.lainio@iki.fi>
2023-05-25 16:29:58 +0300

    rwmap documentation

--- internal/x/rwmap.go
+++ internal/x/rwmap.go
@@ -2,13 +2,27 @@ package x
 
 import "sync"
 
+// TMap is a helper type for the map type RWMap is using. Usually you use the
+// type by definining your own type alias:
+//
+//	type testersMap = x.TMap[int, testing.TB]
+//
+// Which shortens type usage:
+//
+//	x.Tx(testers, func(m testersMap) {
+//	    delete(m, goid())
+//	})
 type TMap[T comparable, U any] map[T]U
 
+// RWMap is a type for a thread-safe Go map. It tries to be short and simple.
 type RWMap[T comparable, U any] struct {
 	sync.RWMutex
 	m TMap[T, U]
 }
 
+// NewRWMap creates a new thread-safe map that's as simple as possible. The
+// first version had only two functions Tx and Rx to allow interact with the
+// map.
 func NewRWMap[T comparable, U any](size ...int) *RWMap[T, U] {
 	// build in make() have to deal by us
 	switch len(size) {
@@ -19,12 +33,16 @@ func NewRWMap[T comparable, U any](size ...int) *RWMap[T, U] {
 	}
 }
 
+// Tx executes a critical section during the function given as an argument. This
+// critical section allows the map be updated. If you only need to read the map
+// please use the Rx function that's for a read-only critical section.
 func Tx[T comparable, U any](m *RWMap[T, U], f func(m TMap[T, U])) {
 	m.Lock()
 	defer m.Unlock()
 	f(m.m)
 }
 
+// Set sets a key value pair to the map.
 func Set[T comparable, U any](m *RWMap[T, U], key T, val U) U {
 	m.Lock()
 	defer m.Unlock()
@@ -32,6 +50,7 @@ func Set[T comparable, U any](m *RWMap[T, U], key T, val U) U {
 	return val
 }
 
+// Del deletes a key value pair from the map.
 func Del[T comparable, U any](m *RWMap[T, U], key T) U {
 	m.Lock()
 	defer m.Unlock()
