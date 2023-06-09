4c6892ff4f013558c752ae03ce290f3b70d6073b
Harri Lainio <harri.lainio@iki.fi>
2023-06-09 18:21:08 +0300

    using methods for rwmap

--- internal/x/rwmap.go
+++ internal/x/rwmap.go
@@ -33,14 +33,14 @@ func NewRWMap[M ~map[T]U, T comparable, U any](size ...int) *RWMap[M, T, U] {
 // Tx executes a critical section during the function given as an argument. This
 // critical section allows the map be updated. If you only need to read the map
 // please use the Rx function that's for a read-only critical section.
-func Tx[M ~map[T]U, T comparable, U any](m *RWMap[M, T, U], f func(m M)) {
+func (m *RWMap[M, T, U]) Tx(f func(m M)) {
 	m.Lock()
 	defer m.Unlock()
 	f(m.m)
 }
 
 // Set sets a key value pair to the map.
-func Set[M ~map[T]U, T comparable, U any](m *RWMap[M, T, U], key T, val U) U {
+func (m *RWMap[M, T, U]) Set(key T, val U) U {
 	m.Lock()
 	defer m.Unlock()
 	m.m[key] = val
@@ -48,7 +48,7 @@ func Set[M ~map[T]U, T comparable, U any](m *RWMap[M, T, U], key T, val U) U {
 }
 
 // Del deletes a key value pair from the map.
-func Del[M ~map[T]U, T comparable, U any](m *RWMap[M, T, U], key T) U {
+func (m *RWMap[M, T, U]) Del(key T) U {
 	m.Lock()
 	defer m.Unlock()
 	val, ok := m.m[key]
@@ -58,13 +58,13 @@ func Del[M ~map[T]U, T comparable, U any](m *RWMap[M, T, U], key T) U {
 	return val
 }
 
-func Rx[M ~map[T]U, T comparable, U any](m *RWMap[M, T, U], f func(m M)) {
+func (m *RWMap[M, T, U]) Rx(f func(m M)) {
 	m.RLock()
 	defer m.RUnlock()
 	f(m.m)
 }
 
-func Get[M ~map[T]U, T comparable, U any](m *RWMap[M, T, U], key T) U {
+func (m *RWMap[M, T, U]) Get(key T) U {
 	m.RLock()
 	defer m.RUnlock()
 	return m.m[key]
