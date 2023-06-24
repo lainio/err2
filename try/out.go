bd9c83bcbef16f25d123703729190cebe12947fc
Harri Lainio <harri.lainio@iki.fi>
2023-06-24 18:15:59 +0300

    try.Out1 implementation and testing started

--- try/out.go
+++ try/out.go
@@ -21,38 +21,54 @@ type (
 		IsThrowf(err error, f string, a ...any) handler // op
 	}
 
-	// TODO: new name, EOpt1?, that we could use it as its own.
-	op1[T any] struct {
+	Result1[T any] struct {
 		Val1 T
 		err  error
 	}
 
-	op2[T any, U any] struct {
+	Result2[T any, U any] struct {
 		Val1 T
 		Val2 U
 		err  error
 	}
 )
 
-func (o *op1[T]) Logf(f string, a ...any) *op1[T] {
+func (o *Result1[T]) Logf(f string, a ...any) *Result1[T] {
 	if o.err == nil {
 		return o
 	}
 	w := tracer.Error.Tracer()
 	if w != nil {
-		fmt.Fprintf(w, f, a...)
+		fmt.Fprintf(w, f+": %v\n", append(a, o.err)...)
 	}
 	return o
 }
 
-func (o *op1[T]) Is(err error) (T, bool) {
+func (o *Result1[T]) Throwf(f string, a ...any) *Result1[T] {
+	if o.err == nil {
+		return o
+	}
+	o.err = fmt.Errorf(f+wrapStr(), append(a, o.err)...)
+	panic(o.err)
+}
+
+// TODO: is this really nessessary? should we rename isDo and offer only it?
+func (o *Result1[T]) Is(err error) (T, bool) {
 	if err == nil || o.err == nil {
 		return o.Val1, err == nil && o.err == nil
 	}
 	return o.Val1, Is(o.err, err)
 }
 
-func (o *op1[T]) IsDo(err error, f ErrFn) *op1[T] {
+func (o *Result1[T]) Def1(v T) *Result1[T] {
+	if o.err == nil {
+		return o
+	}
+	o.Val1 = v
+	return o
+}
+
+func (o *Result1[T]) IsDo(err error, f ErrFn) *Result1[T] {
 	if o.err == nil {
 		return o
 	}
@@ -63,7 +79,7 @@ func (o *op1[T]) IsDo(err error, f ErrFn) *op1[T] {
 }
 
 // We could have Catch that don't panic even the err != nil still
-func (o *op1[T]) Handle(f ErrFn) *op1[T] {
+func (o *Result1[T]) Handle(f ErrFn) *Result1[T] {
 	if f != nil {
 		o.err = f(o.err)
 		if o.err != nil {
@@ -82,13 +98,14 @@ func (o *op1[T]) Handle(f ErrFn) *op1[T] {
 // or in some other cases this would be desired action:
 //
 //	number := try.Out1(strconv.Atoi(str)).Def1(100).Val1
-//
-//nolint:revive
-func Out1[T any](v T, err error) *op1[T] {
-	return &op1[T]{Val1: v, err: err}
+func Out1[T any](v T, err error) *Result1[T] {
+	return &Result1[T]{Val1: v, err: err}
 }
 
-//nolint:revive
-func Out2[T any, U any](v1 T, v2 U, err error) *op2[T, U] {
-	return &op2[T, U]{Val1: v1, Val2: v2, err: err}
+func Out2[T any, U any](v1 T, v2 U, err error) *Result2[T, U] {
+	return &Result2[T, U]{Val1: v1, Val2: v2, err: err}
+}
+
+func wrapStr() string {
+	return ": %w"
 }
--- try/out_test.go
+++ try/out_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 
 	"github.com/lainio/err2"
 	"github.com/lainio/err2/try"
@@ -19,11 +20,12 @@ func ExampleOut1_copyFile() {
 		w := try.To1(os.Create(dst))
 
 		// If you prefer immediate error handling for some reason.
-		_ = try.Out1(io.Copy(w, r)).Handle(func(err error) error {
-			w.Close()
-			os.Remove(dst)
-			return err
-		}).Val1 // and if success, you can use the Copy's retval
+		try.Out1(io.Copy(w, r)).
+			Handle(func(err error) error {
+				w.Close()
+				os.Remove(dst)
+				return err
+			})
 
 		w.Close()
 		return nil
@@ -34,4 +36,28 @@ func ExampleOut1_copyFile() {
 		fmt.Println(err)
 	}
 	// Output: copy /notfound/path/file.go /notfound/path/file.bak: open /notfound/path/file.go: no such file or directory
+}
+
+func ExampleResult1_Def1() {
+	countSomething := func(s string) int {
+		return try.Out1(strconv.Atoi(s)).Def1(100).Val1
+	}
+	num1 := countSomething("1")
+	num2 := countSomething("not number, getting default (=100)")
+	fmt.Printf("results: %d, %d", num1, num2)
+	// Output: results: 1, 100
+}
+
+func ExampleResult1_Logf() {
+	// Set error tracing to stdout that we can see it in Example output. In
+	// normal cases that would be a Logging stream or stderr.
+	err2.SetTracers(os.Stdout)
+
+	countSomething := func(s string) int {
+		return try.Out1(strconv.Atoi(s)).Logf("not number").Def1(100).Val1
+	}
+	num1 := countSomething("1")
+	num2 := countSomething("WRONG")
+	fmt.Printf("results: %d, %d", num1, num2)
+	// not number: strconv.Atoi: parsing "WRONG": invalid syntax Output: results: 1, 100
 }
