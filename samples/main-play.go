491fdc5281557cbd5ae3153bd4bbd9fac623c26b
Harri Lainio <harri.lainio@iki.fi>
2024-01-02 18:40:41 +0200

    added ClassicFileCopy

--- samples/main-play.go
+++ samples/main-play.go
@@ -37,6 +37,31 @@ func CopyFile(src, dst string) (err error) {
 	return nil
 }
 
+func ClassicCopyFile(src, dst string) error {
+	r, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("copy %s %s: %v", src, dst, err)
+	}
+	defer r.Close()
+
+	w, err := os.Create(dst)
+	if err != nil {
+		return fmt.Errorf("copy %s %s: %v", src, dst, err)
+	}
+
+	if _, err := io.Copy(w, r); err != nil {
+		w.Close()
+		os.Remove(dst)
+		return fmt.Errorf("copy %s %s: %v", src, dst, err)
+	}
+
+	if err := w.Close(); err != nil {
+		os.Remove(dst)
+		return fmt.Errorf("copy %s %s: %v", src, dst, err)
+	}
+	return nil
+}
+
 // OrgCopyFile copies the source file to the given destination. If any error occurs it
 // returns an error value describing the reason.
 func OrgCopyFile(src, dst string) (err error) {
